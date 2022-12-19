51b6ede78156deb2c8b1fabf470441b4365bd2ea
Özgün Özerk <ozgun@subspace.network>
2022-12-19 10:39:42 +0300

    rust features

--- config.lua
+++ config.lua
@@ -134,6 +134,103 @@ lvim.builtin.treesitter.highlight.enable = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "stylua", filetypes = { "lua" } },
+  { command = "prettier", filetypes = { "typescript", "javascript" } }
+}
+
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+local codelldb_adapter = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = mason_path .. "bin/codelldb",
+    args = { "--port", "${port}" },
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  },
+}
+
+pcall(function()
+  require("rust-tools").setup {
+    tools = {
+      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+      reload_workspace_from_cargo_toml = true,
+      runnables = {
+        use_telescope = true,
+      },
+      inlay_hints = {
+        auto = true,
+        only_current_line = false,
+        show_parameter_hints = false,
+        parameter_hints_prefix = "<-",
+        other_hints_prefix = "=>",
+        max_len_align = false,
+        max_len_align_padding = 1,
+        right_align = false,
+        right_align_padding = 7,
+        highlight = "Comment",
+      },
+      hover_actions = {
+        border = "rounded",
+      },
+      on_initialized = function()
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+          pattern = { "*.rs" },
+          callback = function()
+            local _, _ = pcall(vim.lsp.codelens.refresh)
+          end,
+        })
+      end,
+    },
+    server = {
+      on_attach = function(client, bufnr)
+        require("lvim.lsp").common_on_attach(client, bufnr)
+        local rt = require "rust-tools"
+        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+      end,
+
+      capabilities = require("lvim.lsp").common_capabilities(),
+      settings = {
+        ["rust-analyzer"] = {
+          lens = {
+            enable = true,
+          },
+          checkOnSave = {
+            enable = true,
+            command = "clippy",
+          },
+        },
+      },
+    },
+  }
+end)
+
+-- vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+lvim.keys.normal_mode["<S-q>"] = ":RustOpenExternalDocs<CR>"
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Rust",
+  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+  R = {
+    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+    "Reload Workspace",
+  },
+  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+}
+
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 -- local formatters = require "lvim.lsp.null-ls.formatters"
 -- formatters.setup {
@@ -173,9 +270,30 @@ lvim.plugins = {
   {
     'eandrju/cellular-automaton.nvim',
     "EdenEast/nightfox.nvim",
-    { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-    -- "folke/trouble.nvim",
-    -- cmd = "TroubleToggle",
+    { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' },
+    "simrat39/rust-tools.nvim",
+    {
+      "saecki/crates.nvim",
+      tag = "v0.3.0",
+      requires = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("crates").setup {
+          null_ls = {
+            enabled = true,
+            name = "crates.nvim",
+          },
+          popup = {
+            border = "rounded",
+          },
+        }
+      end,
+    },
+    {
+      "j-hui/fidget.nvim",
+      config = function()
+        require("fidget").setup()
+      end,
+    },
   },
 }
 
@@ -197,7 +315,7 @@ require("nightfox").setup({
       comments = "italic",
       keywords = "bold, italic",
       types = "italic",
-    }
+    },
   }
 })
 
